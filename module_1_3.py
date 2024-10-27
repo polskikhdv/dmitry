ccda2d203e83a43826df4d2c1a456349a8f70f1b
polskikhdv <polskikhdv@icloud.com>
2024-10-27 11:30:41 +0500

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = 'Дмитрий'
 print("Name:", name)
 age = 31
 print("Age:", age)
-new_age = age + 1
-print("New age:", new_age)
+age = age + 1
+print("New age:", age)
 is_student = True
 print("Is student:", is_student)
