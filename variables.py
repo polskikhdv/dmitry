b297110f0ea32042178d416d5eb060a6af95ded1
polskikhdv <polskikhdv@icloud.com>
2024-10-28 19:55:20 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+homework_quantity = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = hours_spent / homework_quantity
+print('Курс:', course_name,', ''всего задач:', homework_quantity, ',','среднее время выполнения:', time_for_one_task,'.')
